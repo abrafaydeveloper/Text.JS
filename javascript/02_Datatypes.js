dc3626131276ebd05572ad33060fa8d6b66eb74b
Abdul Rafay <abdulrafayofficial93@gmail.com>
2024-10-17 22:00:37 +0000

    Datatypes

---
+++ javascript/02_Datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // Treat all JS Code as newer version
+
+// alert(2+3) // we are using node js , not browser
+
+// console.log(3+3); console.log("Rafay");
+// console.log(3 + 
+//                3)   code readability should be high
+
+// like that :- 
+
+console.log("Abdul Rafay")
+console.log(3+4)
+
+let num = null;
+let name;
+
+// Data Types In JS
+
+/*  1. Number => 2 to power 53 (not confirm)
+    2. bigint => Using for high number values stored in memory
+    3. string => ""
+    4. null => standalone value
+    5. undefined 
+    6. Symbol => Find Unique Components
+    7. Objects 
+    8.boolean => True/False
+    */
+
+
+    console.log(typeof num);  // Object
+    console.log(typeof name); // Undefined
+    
+    
+    
+
