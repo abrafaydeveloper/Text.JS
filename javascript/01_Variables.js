bf9bfcf1d42ad96f26d0443c2353623045cdca42
Abdul Rafay <abdulrafayofficial93@gmail.com>
2024-10-17 21:59:30 +0000

    Variables

---
+++ javascript/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = "Abdul Rafay";
+let accountEmail = "abdulrafayofficial93@gmail.com";
+var accountPassword = "12345";
+accountCity = "Karachi";
+
+accountEmail = "home1122";
+
+accountCity = "Sanghar";
+
+/* Don't use var variables
+   because of issue in block scope and functional scope  */
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
